f4d741f4a67064ee07e82cb8085f9ea10ba93cdc
Jules Youngberg <julesdayoungberg@gmail.com>
2020-10-03 02:15:49 -0700

    eslint + prettier

--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -3,22 +3,22 @@ import Head from 'next/head';
 import Link from 'next/link';
 
 const Home = () => {
-  return (
-    <React.Fragment>
-      <Head>
-        <title>Home - Nextron (with-typescript)</title>
-      </Head>
-      <div>
-        <p>
-          ⚡ Electron + Next.js ⚡ -
-          <Link href="/next">
-            <a>Go to next page</a>
-          </Link>
-        </p>
-        <img src="/images/logo.png" />
-      </div>
-    </React.Fragment>
-  );
+    return (
+        <>
+            <Head>
+                <title>Home - Nextron (with-typescript)</title>
+            </Head>
+            <div>
+                <p>
+                    Electron + Next.js -
+                    <Link href='/next'>
+                        <a>Go to next page</a>
+                    </Link>
+                </p>
+                <img src='/images/logo.png' />
+            </div>
+        </>
+    );
 };
 
 export default Home;
--- renderer/pages/next.tsx
+++ renderer/pages/next.tsx
@@ -3,21 +3,21 @@ import Head from 'next/head';
 import Link from 'next/link';
 
 const Next = () => {
-  return (
-    <React.Fragment>
-      <Head>
-        <title>Next - Nextron (with-typescript)</title>
-      </Head>
-      <div>
-        <p>
-          ⚡ Electron + Next.js ⚡ -
-          <Link href="/home">
-            <a>Go to home page</a>
-          </Link>
-        </p>
-      </div>
-    </React.Fragment>
-  );
+    return (
+        <>
+            <Head>
+                <title>Next - Nextron (with-typescript)</title>
+            </Head>
+            <div>
+                <p>
+                    ⚡ Electron + Next.js ⚡ -
+                    <Link href='/home'>
+                        <a>Go to home page</a>
+                    </Link>
+                </p>
+            </div>
+        </>
+    );
 };
 
 export default Next;
